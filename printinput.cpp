78eb23dc74b358c6cbdd3ffb55b6fd069e6af3aa
Papershine <hilarylau12342@gmail.com>
2023-01-16 21:34:02 -0800

    Add printinput.cpp

---
+++ printinput.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Enter a number: ";
+
+    int num{};
+    std::cin >> num;
+
+    std::cout << "You entered the number " << num;
+
+    return 0;
+}
